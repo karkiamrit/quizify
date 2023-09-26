30e0c6aeac8441d0da8abc85e4a79d3516f29098
karkiamrit <amrit.karki2073@gmail.com>
2023-09-26 12:25:06 +0545

    chatgpt configuration setup

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,7 +1,43 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import * as Zod from 'zod';
+import { strict_output } from "@/lib/gpt";
+import { ZodError } from "zod";
 
-export const POST=async (req:Request,res:Response)=>{
-    return NextResponse.json({
-        hello:'world'
-    })
+// POST /api/questions
+export const POST = async (req: Request, res: Response) => {
+    try {
+        const body = await req.json();
+        const { amount, topic, type } = quizCreationSchema.parse(body);
+        let questions: any;
+
+        if (type === 'open_ended') {
+            questions = await strict_output(
+                'You are a helpful AI that is able to generate a pair of questions and answers, the length of the answer should not exceed 15 words, store all pairs of answers and questions in a JSON way',
+                new Array(amount).fill(`You are to generate a random hard open-ended question about ${topic}`),
+                {
+                    question: 'question',
+                    answer: 'answer with a maximum length of 15 words'
+                }
+            );
+        }
+
+        return NextResponse.json({
+            questions,
+        }, {
+            status: 200
+        });
+    } catch (err) {
+        if (err instanceof ZodError) {
+            return NextResponse.json({
+                err: err.issues
+            }, {
+                status: 400
+            });
+        }
+    }
+    return new Response("An unexpected error occurred", {
+        status: 500 // Set an appropriate error status code
+    });
+    
 }
--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY as string
 });
  
 interface OutputFormat {
